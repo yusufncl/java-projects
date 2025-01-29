505da91d2be8f66ecd2a9c23f173887c26fe5a77
yusuf <y.a.rouf2@newcastle.ac.uk>
2025-01-29 01:26:12 +0000

    Initial commit

---
+++ Data Structures I/validPalindrome.java
@@ -0,0 +1,12 @@
+class Solution{
+    public static void main(String[] args) {
+        
+    }
+
+    public boolean isPalindrome(String s) {
+        
+        
+
+    }
+
+}
---
+++ The Casino/Blackjack.java
@@ -0,0 +1,21 @@
+/* Welcome to The Casino
+ * Today's game of choice is: Blackjack
+ */
+
+ import java.util.Scanner;
+
+ public class Blackjack {
+    Player();
+ }
+
+ class Player {
+    Scanner playerName = new Scanner(System.in);
+    String userName;
+
+    System.out.println("Enter username: ");
+    
+ }
+
+ class Dealer {
+
+ }
